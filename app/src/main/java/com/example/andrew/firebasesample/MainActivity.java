325d26be1e2d2e5bc6bb13480f943a32dd793b56
Andrew Bae Sangwoo <andrew@ggmsoft.com>
2017-07-20 18:02:52 +0900

    Add new activities

--- app/src/main/java/com/example/andrew/firebasesample/MainActivity.java
+++ app/src/main/java/com/example/andrew/firebasesample/MainActivity.java
@@ -4,10 +4,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,7 +29,8 @@ import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
@@ -32,8 +41,11 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener mAuthListener;
     private FirebaseAnalytics mFirebaseAnalytics;
 
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawer;
+    private NavigationView mNavigation;
+    private TextView tvNavHeaderEmail, tvNavHeaderDisplayName;
     private TextView tvInfo;
-    private Button btnSignOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,20 +54,21 @@ public class MainActivity extends AppCompatActivity {
 
         mContext = this;
 
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(this);
+
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mNavigation = (NavigationView) findViewById(R.id.nav_view);
+
         tvInfo = (TextView) findViewById(R.id.tv_info);
-        btnSignOut = (Button) findViewById(R.id.btn_sign_out);
-        btnSignOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signOut();
-            }
-        });
+
+        initNavigationDrawer();
 
         initGoogleSignIn();
         initFirebaseAuth();
-        initFirebaseAnalytics();
-
-        sendEventForAnalytics();
     }
 
     @Override
@@ -97,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
-                    tvInfo.setText(user.getEmail());
+                    updateUI(user);
                 } else {
                     Toast.makeText(mContext, "onAuthChanged: sign out", Toast.LENGTH_SHORT).show();
                 }
@@ -105,19 +118,24 @@ public class MainActivity extends AppCompatActivity {
         };
     }
 
-    private void initFirebaseAnalytics() {
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+    private void initNavigationDrawer() {
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        mNavigation.setNavigationItemSelectedListener(this);
+
+        View header = mNavigation.getHeaderView(0);
+
+        tvNavHeaderDisplayName = (TextView) header.findViewById(R.id.tv_display_name);
+        tvNavHeaderEmail = (TextView) header.findViewById(R.id.tv_email);
     }
 
-    private void sendEventForAnalytics() {
-        String id = "id";
-        String name = "name";
-
-        Bundle bundle = new Bundle();
-        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, id);
-        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, name);
-        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image");
-        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+    private void updateUI(FirebaseUser user) {
+        tvNavHeaderEmail.setText(user.getEmail());
+        tvNavHeaderDisplayName.setText(user.getDisplayName());
+        tvInfo.setText(user.getEmail());
     }
 
     private void signOut() {
@@ -136,5 +154,66 @@ public class MainActivity extends AppCompatActivity {
         nextIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(nextIntent);
         finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawer.isDrawerOpen(GravityCompat.START)) {
+            mDrawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                break;
+            case R.id.action_sign_out:
+                signOut();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        switch (item.getItemId()) {
+            case R.id.nav_analytics:
+                Toast.makeText(mContext, R.string.nav_analytics, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_database:
+                Toast.makeText(mContext, R.string.nav_database, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_notification:
+                Toast.makeText(mContext, R.string.nav_notification, Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        mDrawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fab:
+                Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/andrew/firebasesample/services/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/andrew/firebasesample/services/MyFirebaseMessagingService.java
@@ -36,7 +36,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private void sendNotification(String messageTitle, String messageBody) {
         Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
 
         Notification notification = new NotificationCompat.Builder(this)
